79e04625d5fe8a6f86f10f9c81e4a3af942ae2e6
jarverha <jarne.Verhaeghe@ugent.be>
2022-03-16 17:32:38 +0100

    Renaming function arguments, Splitting up the function to the base_function.py

--- Base/base_functions.py
+++ Base/base_functions.py
@@ -1,4 +1,7 @@
 from scipy import stats
+from statsmodels.stats.power import TTestPower
+import pandas as pd
+import numpy as np
 
 
 def p_values_null_coef(coefficients):
@@ -7,5 +10,89 @@ def p_values_null_coef(coefficients):
 
 
 def p_values_arg_coef(coefficients, arg):
-    # inverse of quantile
-    return stats.percentileofscore(coefficients, arg)
+	# inverse of quantile
+	return stats.percentileofscore(coefficients, arg)
+
+def powerSHAP_statistical_analysis(shaps_df,power_alpha,power_req_iterations,include_all):
+	p_values = []
+	effect_size = []
+	power_list = []
+	required_iterations = []
+	n_samples = len(shaps_df["random_uniform_feature"].values)
+	mean_random_uniform = shaps_df["random_uniform_feature"].values.mean()
+	for i in range(len(shaps_df.columns)):
+	    quantile = (
+	        p_values_arg_coef(
+	            np.array(shaps_df.values[:, i]), mean_random_uniform
+	        )
+	        / 100
+	    )
+	    p_value = quantile
+	    p_values.append(p_value)
+
+	    if include_all or p_value < power_alpha:
+	        pooled_standard_deviation = np.sqrt(
+	            (
+	                (shaps_df.std()[i] ** 2) * (n_samples - 1)
+	                + (n_samples - 1)
+	                * (shaps_df["random_uniform_feature"].values.std() ** 2)
+	            )
+	            / (n_samples * 2 - 2)
+	        )
+	        effect_size.append(
+	            (np.abs(shaps_df.mean()[i] - mean_random_uniform))
+	            / pooled_standard_deviation
+	        )
+	        power_list.append(
+	            TTestPower().power(
+	                effect_size=effect_size[-1],
+	                nobs=n_samples,
+	                alpha=power_alpha,
+	                df=None,
+	                alternative="larger",
+	            )
+	        )
+	        if shaps_df.columns[i] == "random_uniform_feature":
+	            required_iterations.append(0)
+	        else:
+	            required_iterations.append(
+	                TTestPower().solve_power(
+	                    effect_size=effect_size[-1],
+	                    nobs=None,
+	                    alpha=power_alpha,
+	                    power=power_req_iterations,
+	                    alternative="larger",
+	                )
+	            )
+
+	    else:
+	        required_iterations.append(0)
+	        effect_size.append(0)
+	        power_list.append(0)
+
+	processed_shaps_df = pd.DataFrame(
+	    data=np.hstack(
+	        [
+	            np.reshape(shaps_df.mean().values, (-1, 1)),
+	            np.reshape(np.array(p_values), (len(p_values), 1)),
+	            np.reshape(np.array(effect_size), (len(effect_size), 1)),
+	            np.reshape(np.array(power_list), (len(power_list), 1)),
+	            np.reshape(
+	            	np.array(required_iterations), (len(required_iterations), 1)
+	            ),
+	        ]
+	    ),
+	    columns=[
+	        "impact",
+	        "p_value",
+	        "effect_size",
+	        "power_"+str(power_alpha)+"_alpha",
+	        str(power_req_iterations)+"_power_its_req",
+		],
+		index=shaps_df.mean().index,
+	)
+	processed_shaps_df = processed_shaps_df.reindex(
+		processed_shaps_df.impact.abs().sort_values(ascending=False).index
+	)
+
+	return processed_shaps_df
--- TreeExplainer/CatBoost.py
+++ TreeExplainer/CatBoost.py
@@ -5,29 +5,29 @@ import shap
 from tqdm import tqdm
 from numpy.random import RandomState
 from PowerSHAP.Base import base_functions
-from statsmodels.stats.power import TTestPower
 
 
 ########################################################################################################################
-# Inp_db: Input DataFrame for training (Pandas Dataframe)
-# Feature_cols: The feature columns used in the Inp_db DataFrame (List)
+# input_df: Input DataFrame for training (Pandas Dataframe)
+# feature_columns: The feature columns used in the input_df DataFrame (List)
 # powershap_iterations: Indicating the amount of shuffles and iterations of the method, ignored when using automatic mode (int)
-# Index: The index column of the Inp_db Dataframe, this is used to also split the database into a train and validation set (String)
+# index_column: The index column of the input_df Dataframe, this is used to also split the database into a train and validation set (String)
+# target_column: The target column of the input_df Dataframe (String)
 # model: The model used for the iteration (CatBoost model)
 # val_size: The fractional validation set size (float between [0,1])
 # stratify: Stratify the train_test_split (Boolean)
-# include_all: If false, this method will result all features with a threshold of alpha and not save all shap_values and corresponding data samples (Boolean)
+# include_all: If false, this method will output all features with a threshold of power_alpha (Boolean)
 # power_alpha: The alpha value used for the power-calculation of the used statistical test and significance threshold (float between ]0,1[)
 # power_req_iterations: The fractional power percentage for the required iterations calculation
 # automatic: If true, the PowerSHAP will first calculate the required iterations by using ten iterations and then restart using the required iterations for power_alpha (Boolean)
-# limit_recursive_automatic: restricts the amount of automatic PowerSHAP recursion (int)
 # limit_automatic: sets a limit to the maximum allowed iterations (int)
+# limit_recursive_automatic: restricts the amount of automatic PowerSHAP recursion (int)
 def PowerSHAP(
-    Inp_db,
-    Feature_cols,
-    Target,
-    Index,
+    input_df,
     model,
+    target_column,
+    feature_columns=None,
+    index_column=None,
     powershap_iterations=10,
     val_size=0.2,
     stratify=None,
@@ -44,14 +44,21 @@ def PowerSHAP(
     Shap_values_ar = np.array([])
     X_Shap_ar = np.array([])
 
-    current_df = Inp_db.copy(deep=True)
+    current_df = input_df.copy(deep=True)
 
-    columns_list_current = Feature_cols + ["random_uniform_feature"]
+    if index_column == None:
+        current_df = current_df.reset_index()
+        index_column = "index"
+
+    if feature_columns == None:
+        feature_columns = list(current_df.drop(columns=[target_column,index_column]).columns.values)
+
+    columns_list_current = feature_columns + ["random_uniform_feature"]
 
     if automatic:
         assert (
             limit_automatic != None
-        ), "limit_automatic must be specified when automatic mode is used"
+        ), "\"limit_automatic\" must be specified when automatic mode is used"
         loop_its = 10
         print(
             "Automatic mode enabled: Finding the minimal required PowerSHAP iterations for significance of "
@@ -68,19 +75,19 @@ def PowerSHAP(
         current_df["random_uniform_feature"] = random_uniform_feature
 
         train_idx, val_idx = train_test_split(
-            current_df[Index].unique(),
+            current_df[index_column].unique(),
             test_size=val_size,
             random_state=i,
             stratify=stratify,
         )
 
-        X_train = current_df[current_df[Index].isin(train_idx)].copy(deep=True)
-        X_val = current_df[current_df[Index].isin(val_idx)].copy(deep=True)
+        X_train = current_df[current_df[index_column].isin(train_idx)].copy(deep=True)
+        X_val = current_df[current_df[index_column].isin(val_idx)].copy(deep=True)
 
         ## Calculate the required labels
 
-        Y_train = X_train[Target].values
-        Y_val = X_val[Target].values
+        Y_train = X_train[target_column].values
+        Y_val = X_val[target_column].values
 
         ## Extract the required features
         X_train_feat = X_train[columns_list_current]
@@ -93,13 +100,13 @@ def PowerSHAP(
 
         Shap_values = C_explainer.shap_values(X_val_feat)
 
-        if include_all:
-            if len(Shap_values_ar) > 0:
-                Shap_values_ar = np.vstack([Shap_values_ar, Shap_values])
-                X_Shap_ar = np.vstack([X_Shap_ar, X_val_feat])
-            else:
-                Shap_values_ar = Shap_values
-                X_Shap_ar = X_val_feat
+        # if include_all:
+        #     if len(Shap_values_ar) > 0:
+        #         Shap_values_ar = np.vstack([Shap_values_ar, Shap_values])
+        #         X_Shap_ar = np.vstack([X_Shap_ar, X_val_feat])
+        #     else:
+        #         Shap_values_ar = Shap_values
+        #         X_Shap_ar = X_val_feat
 
         Shap_values = np.abs(Shap_values)
 
@@ -108,96 +115,98 @@ def PowerSHAP(
         else:
             Shaps = np.mean(Shap_values, axis=0)
 
-    shaps_pd = pd.DataFrame(data=Shaps, columns=columns_list_current)
+    shaps_df = pd.DataFrame(data=Shaps, columns=columns_list_current)
 
     ############################################################################
     ## SHAP STATISTICAL ANALYSIS
-    p_values = []
-    effect_size = []
-    power_list = []
-    required_iterations = []
-    n_samples = len(shaps_pd["random_uniform_feature"].values)
-    mean_random_uniform = shaps_pd["random_uniform_feature"].values.mean()
-    for i in range(len(shaps_pd.columns)):
-        quantile = (
-            base_functions.p_values_arg_coef(
-                np.array(shaps_pd.values[:, i]), mean_random_uniform
-            )
-            / 100
-        )
-        p_value = quantile
-        p_values.append(p_value)
+    # p_values = []
+    # effect_size = []
+    # power_list = []
+    # required_iterations = []
+    # n_samples = len(shaps_df["random_uniform_feature"].values)
+    # mean_random_uniform = shaps_df["random_uniform_feature"].values.mean()
+    # for i in range(len(shaps_df.columns)):
+    #     quantile = (
+    #         base_functions.p_values_arg_coef(
+    #             np.array(shaps_df.values[:, i]), mean_random_uniform
+    #         )
+    #         / 100
+    #     )
+    #     p_value = quantile
+    #     p_values.append(p_value)
 
-        if include_all or p_value < power_alpha:
-            pooled_standard_deviation = np.sqrt(
-                (
-                    (shaps_pd.std()[i] ** 2) * (n_samples - 1)
-                    + (n_samples - 1)
-                    * (shaps_pd["random_uniform_feature"].values.std() ** 2)
-                )
-                / (n_samples * 2 - 2)
-            )
-            effect_size.append(
-                (np.abs(shaps_pd.mean()[i] - mean_random_uniform))
-                / pooled_standard_deviation
-            )
-            power_list.append(
-                TTestPower().power(
-                    effect_size=effect_size[-1],
-                    nobs=n_samples,
-                    alpha=power_alpha,
-                    df=None,
-                    alternative="larger",
-                )
-            )
-            if shaps_pd.columns[i] == "random_uniform_feature":
-                required_iterations.append(0)
-            else:
-                required_iterations.append(
-                    TTestPower().solve_power(
-                        effect_size=effect_size[-1],
-                        nobs=None,
-                        alpha=power_alpha,
-                        power=power_req_iterations,
-                        alternative="larger",
-                    )
-                )
+    #     if include_all or p_value < power_alpha:
+    #         pooled_standard_deviation = np.sqrt(
+    #             (
+    #                 (shaps_df.std()[i] ** 2) * (n_samples - 1)
+    #                 + (n_samples - 1)
+    #                 * (shaps_df["random_uniform_feature"].values.std() ** 2)
+    #             )
+    #             / (n_samples * 2 - 2)
+    #         )
+    #         effect_size.append(
+    #             (np.abs(shaps_df.mean()[i] - mean_random_uniform))
+    #             / pooled_standard_deviation
+    #         )
+    #         power_list.append(
+    #             TTestPower().power(
+    #                 effect_size=effect_size[-1],
+    #                 nobs=n_samples,
+    #                 alpha=power_alpha,
+    #                 df=None,
+    #                 alternative="larger",
+    #             )
+    #         )
+    #         if shaps_df.columns[i] == "random_uniform_feature":
+    #             required_iterations.append(0)
+    #         else:
+    #             required_iterations.append(
+    #                 TTestPower().solve_power(
+    #                     effect_size=effect_size[-1],
+    #                     nobs=None,
+    #                     alpha=power_alpha,
+    #                     power=power_req_iterations,
+    #                     alternative="larger",
+    #                 )
+    #             )
 
-        else:
-            required_iterations.append(0)
-            effect_size.append(0)
-            power_list.append(0)
+    #     else:
+    #         required_iterations.append(0)
+    #         effect_size.append(0)
+    #         power_list.append(0)
 
-    processed_shaps_pd = pd.DataFrame(
-        data=np.hstack(
-            [
-                np.reshape(shaps_pd.mean().values, (-1, 1)),
-                np.reshape(np.array(p_values), (len(p_values), 1)),
-                np.reshape(np.array(effect_size), (len(effect_size), 1)),
-                np.reshape(np.array(power_list), (len(power_list), 1)),
-                np.reshape(
-                    np.array(required_iterations), (len(required_iterations), 1)
-                ),
-            ]
-        ),
-        columns=[
-            "Impact",
-            "p_value",
-            "effect_size",
-            "power_0_01_alpha",
-            "95_power_its_req",
-        ],
-        index=shaps_pd.mean().index,
-    )
-    processed_shaps_pd = processed_shaps_pd.reindex(
-        processed_shaps_pd.Impact.abs().sort_values(ascending=False).index
-    )
+    # processed_shaps_df = pd.DataFrame(
+    #     data=np.hstack(
+    #         [
+    #             np.reshape(shaps_df.mean().values, (-1, 1)),
+    #             np.reshape(np.array(p_values), (len(p_values), 1)),
+    #             np.reshape(np.array(effect_size), (len(effect_size), 1)),
+    #             np.reshape(np.array(power_list), (len(power_list), 1)),
+    #             np.reshape(
+    #                 np.array(required_iterations), (len(required_iterations), 1)
+    #             ),
+    #         ]
+    #     ),
+    #     columns=[
+    #         "Impact",
+    #         "p_value",
+    #         "effect_size",
+    #         "power_"+str(power_alpha)+"_alpha",
+    #         str(power_req_iterations)+"_power_its_req",
+    #     ],
+    #     index=shaps_df.mean().index,
+    # )
+    # processed_shaps_df = processed_shaps_df.reindex(
+    #     processed_shaps_df.Impact.abs().sort_values(ascending=False).index
+    # )
+
+    processed_shaps_df = base_functions.powerSHAP_statistical_analysis(shaps_df,power_alpha,power_req_iterations,include_all)
 
     if automatic:
         max_iterations = int(
             np.ceil(
-                processed_shaps_pd[processed_shaps_pd.p_value < 0.01][
-                    "95_power_its_req"
+                processed_shaps_df[processed_shaps_df.p_value < power_alpha][
+                    str(power_req_iterations)+"_power_its_req"
                 ].max()
             )
         )
@@ -208,11 +217,11 @@ def PowerSHAP(
                 + str(limit_automatic)
                 + " iterations."
             )
-            processed_shaps_pd = PowerSHAP(
-                Inp_db=Inp_db,
-                Feature_cols=Feature_cols,
-                Target=Target,
-                Index=Index,
+            processed_shaps_df = PowerSHAP(
+                input_df=current_df,
+                feature_columns=feature_columns,
+                target_column=target_column,
+                index_column=index_column,
                 model=model,
                 powershap_iterations=limit_automatic,
                 val_size=val_size,
@@ -235,11 +244,11 @@ def PowerSHAP(
                     + str(max_iterations)
                     + " iterations."
                 )
-                processed_shaps_pd = PowerSHAP(
-                    Inp_db=Inp_db,
-                    Feature_cols=Feature_cols,
-                    Target=Target,
-                    Index=Index,
+                processed_shaps_df = PowerSHAP(
+                    input_df=current_df,
+                    feature_columns=feature_columns,
+                    target_column=target_column,
+                    index_column=index_column,
                     model=model,
                     powershap_iterations=max_iterations,
                     val_size=val_size,
@@ -253,8 +262,8 @@ def PowerSHAP(
                 max_iterations_old = max_iterations
                 max_iterations = int(
                     np.ceil(
-                        processed_shaps_pd[processed_shaps_pd.p_value < 0.01][
-                            "95_power_its_req"
+                        processed_shaps_df[processed_shaps_df.p_value < power_alpha][
+                            str(power_req_iterations)+"_power_its_req"
                         ].max()
                     )
                 )
@@ -262,9 +271,9 @@ def PowerSHAP(
 
     print("Done!")
     if include_all:
-        return processed_shaps_pd  # ,shaps_pd,Shap_values_ar,X_Shap_ar
+        return processed_shaps_df  # ,shaps_df,Shap_values_ar,X_Shap_ar
 
     else:
-        return processed_shaps_pd[
-            processed_shaps_pd.p_value < power_alpha
-        ]  # ,shaps_pd,Shap_values_ar,X_Shap_ar
+        return processed_shaps_df[
+            processed_shaps_df.p_value < power_alpha
+        ]  # ,shaps_df,Shap_values_ar,X_Shap_ar
