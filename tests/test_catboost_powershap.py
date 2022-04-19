c62b1b4d97995e9d22f06829bc18922742f77436
jarverha <jarne.Verhaeghe@ugent.be>
2022-04-19 15:22:52 +0200

    :bug: fixed bug in test_catboost_powershap

--- tests/test_catboost_powershap.py
+++ tests/test_catboost_powershap.py
@@ -68,7 +68,7 @@ def test_catboost_handle_infs(dummy_classification):
     X, y = dummy_classification
     X.iloc[:5] = np.Inf
     X["inf_col"] = np.Inf
-    assert np.any(pd.isna(X))
+    assert np.any(pd.isinf(X))
     n_informative = sum([c.startswith("informative") for c in X.columns])
     assert n_informative > 0, "No informative columns in the dummy data!"
 
@@ -89,6 +89,7 @@ def test_catboost_handle_infs_nans(dummy_classification):
     X.iloc[:10] = None
     X["inf_col"] = np.Inf
     X["nan_col"] = None
+    assert np.any(pd.isinf(X))
     assert np.any(pd.isna(X))
     n_informative = sum([c.startswith("informative") for c in X.columns])
     assert n_informative > 0, "No informative columns in the dummy data!"
