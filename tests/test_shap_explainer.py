36227d09d8a59ebb088a5f7ff138b4565353770f
jervdrdo <jeroen.vanderdonckt@ugent.be>
2022-04-25 12:55:57 +0200

    :fire: extend tests + minor bug fixes + update dependencies

--- tests/test_shap_explainer.py
+++ tests/test_shap_explainer.py
@@ -1,4 +1,6 @@
 import pytest
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.linear_model import SGDRegressor
 
 from powershap.shap_wrappers import ShapExplainerFactory
 from powershap.shap_wrappers.shap_explainer import (
@@ -10,24 +12,81 @@ from powershap.shap_wrappers.shap_explainer import (
 
 
 def test_get_linear_explainer():
-    from sklearn.linear_model import LogisticRegression
+    from sklearn.linear_model import (
+        LogisticRegression,
+        LogisticRegressionCV,
+        PassiveAggressiveClassifier,
+        Perceptron,
+        RidgeClassifier,
+        RidgeClassifierCV,
+        SGDClassifier,
+        LinearRegression,
+        Ridge,
+        RidgeCV,
+        SGDRegressor,
+    )
 
-    explainer = ShapExplainerFactory.get_explainer(LogisticRegression())
-    assert isinstance(explainer, LinearExplainer)
+    model_classes = [
+        # Classifiers
+        LogisticRegression,
+        LogisticRegressionCV,
+        PassiveAggressiveClassifier,
+        Perceptron,
+        RidgeClassifier,
+        RidgeClassifierCV,
+        SGDClassifier,
+        # Regressors
+        LinearRegression,
+        Ridge,
+        RidgeCV,
+        SGDRegressor,
+    ]
+
+    for model_class in model_classes:
+        explainer = ShapExplainerFactory.get_explainer(model_class())
+        assert isinstance(explainer, LinearExplainer)
 
 
 def test_get_catboost_explainer():
-    from catboost import CatBoostClassifier
+    from catboost import CatBoostClassifier, CatBoostRegressor
 
-    explainer = ShapExplainerFactory.get_explainer(CatBoostClassifier())
-    assert isinstance(explainer, CatboostExplainer)
+    model_classes = [CatBoostClassifier, CatBoostRegressor]
+
+    for model_class in model_classes:
+        explainer = ShapExplainerFactory.get_explainer(model_class())
+        assert isinstance(explainer, CatboostExplainer)
 
 
 def test_get_ensemble_explainer():
-    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.ensemble import (
+        RandomForestClassifier,
+        AdaBoostClassifier,
+        GradientBoostingClassifier,
+        ExtraTreesClassifier,
+        # HistGradientBoostingClassifier,
+        RandomForestRegressor,
+        AdaBoostRegressor,
+        GradientBoostingRegressor,
+        ExtraTreesRegressor,
+        # HistGradientBoostingRegressor,
+    )
 
-    explainer = ShapExplainerFactory.get_explainer(RandomForestClassifier())
-    assert isinstance(explainer, EnsembleExplainer)
+    model_classes = [
+        RandomForestClassifier,
+        # AdaBoostClassifier,  # Requires extra check on the base_estimator
+        GradientBoostingClassifier,
+        ExtraTreesClassifier,
+        # HistGradientBoostingClassifier,
+        RandomForestRegressor,
+        # AdaBoostRegressor,  # Requires extra check on the base_estimator
+        GradientBoostingRegressor,
+        ExtraTreesRegressor,
+        # HistGradientBoostingRegressor,
+    ]
+
+    for model_class in model_classes:
+        explainer = ShapExplainerFactory.get_explainer(model_class())
+        assert isinstance(explainer, EnsembleExplainer)
 
 
 def test_get_deep_learning_explainer():
