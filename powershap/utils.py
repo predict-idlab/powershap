2d93972dc55172cabdeb06f1bee06e97efb3b845
jarverha <jarne.Verhaeghe@ugent.be>
2025-09-24 17:16:54 +0200

    inhomogeneous  fix

--- powershap/utils.py
+++ powershap/utils.py
@@ -61,30 +61,34 @@ def powerSHAP_statistical_analysis(
             required_iterations.append(0)
             effect_size.append(0)
             power_list.append(0)
-    try:
-        processed_shaps_df = pd.DataFrame(
-            data=np.hstack(
-                [
-                    np.reshape(shaps_df.mean().values, (-1, 1)),
-                    np.reshape(np.array(p_values), (len(p_values), 1)),
-                    np.reshape(np.array(effect_size), (len(effect_size), 1)),
-                    np.reshape(np.array(power_list), (len(power_list), 1)),
-                    np.reshape(np.array(required_iterations), (len(required_iterations), 1)),
-                ]
-            ),
-            columns=[
-                "impact",
-                "p_value",
-                "effect_size",
-                "power_" + str(power_alpha) + "_alpha",
-                str(power_req_iterations) + "_power_its_req",
-            ],
-            index=shaps_df.mean().index,
-        )
-    except ValueError as e:
-        # If a ValueError occurs, print the error and append a placeholder.
-        print(f"failed with error: {e}")
-        print(f"required iterations is = {required_iterations}")
+    
+    #This code is required because if statsmodels does not converge, due to numpy issues (see closed issue for inhomogeneous shape) the code does not work otherwise
+    flattened_required_iterations = []
+    for item in required_iterations:
+        if isinstance(item, np.ndarray):
+            flattened_required_iterations.extend(item.tolist())
+    else:
+        flattened_required_iterations.append(item)
+
+    processed_shaps_df = pd.DataFrame(
+        data=np.hstack(
+            [
+                np.reshape(shaps_df.mean().values, (-1, 1)),
+                np.reshape(np.array(p_values), (len(p_values), 1)),
+                np.reshape(np.array(effect_size), (len(effect_size), 1)),
+                np.reshape(np.array(power_list), (len(power_list), 1)),
+                np.reshape(np.array(flattened_required_iterations), (len(flattened_required_iterations), 1)),
+            ]
+        ),
+        columns=[
+            "impact",
+            "p_value",
+            "effect_size",
+            "power_" + str(power_alpha) + "_alpha",
+            str(power_req_iterations) + "_power_its_req",
+        ],
+        index=shaps_df.mean().index,
+    )
 
     processed_shaps_df = processed_shaps_df.reindex(
         processed_shaps_df.impact.abs().sort_values(ascending=False).index
